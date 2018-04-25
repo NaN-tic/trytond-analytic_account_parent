31849b96b2c407f6b15125319c1bd45dc9799bd7
?ngel ?lvarez <angel@nan-tic.com>
2018-04-25 11:28:25 +0200

    upgrade to 4.7

--- __init__.py
+++ __init__.py
@@ -2,7 +2,8 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-import account
+from . import account
+
 
 def register():
     Pool.register(
